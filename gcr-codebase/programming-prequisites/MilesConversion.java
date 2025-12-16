c02caf47d4069d1610d5b90bef0fcf2e3ebe3273
rudraraj100 <rudrar2002@gmail.com>
2025-12-16 21:46:10 +0530

    [Rudra Raj] : created a program to convert kilometers into miles

---
+++ gcr-codebase/programming-prequisites/MilesConversion.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+  public class MilesConversion{
+     public static void main(String[] args){
+	    Scanner sc = new Scanner(System.in);
+		
+		double km = sc.nextDouble();
+		
+		double miles = km * 0.621371;
+		
+		System.out.println(miles);
+	}
+}	
+		
