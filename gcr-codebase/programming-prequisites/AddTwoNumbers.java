8a8c6a9100dbbd26ff8be9db6b569ecda0183d3c
rudraraj100 <rudrar2002@gmail.com>
2025-12-16 23:53:09 +0530

    [Rudra Raj] : Created a program to add two numbers.

--- gcr-codebase/programming-prequisites/AddTwoNumbers.java
+++ gcr-codebase/programming-prequisites/AddTwoNumbers.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
   public class AddTwoNumbers{
      public static void main(String[] args){
-	    Scanner sc = next Scanner(System.in);
+	    Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		
