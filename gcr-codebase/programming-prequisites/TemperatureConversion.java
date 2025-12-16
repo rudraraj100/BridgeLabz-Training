477e6a088fc5e0dd3a314ff8208f3eec67887ac9
RUDRA RAJ <91939502+rudraraj100@users.noreply.github.com>
2025-12-17 00:15:08 +0530

    [Rudra Raj] : Created a program to convert celsius into fahrenheit temperature.
    corrected the variable name

--- gcr-codebase/programming-prequisites/TemperatureConversion.java
+++ gcr-codebase/programming-prequisites/TemperatureConversion.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
   public class TemperatureConversion{
     public static void main(String[] args){
-	  Scanner sc = next.Scanner(System.in);
+	  Scanner sc = new Scanner(System.in);
 	  float celsius = sc.nextFloat();
 	  
 	  float fahrenheit = (celsius * 9/5) + 32;
 	  
-	  System.out.println(Fahrenhit + " 'F");
+	  System.out.println(fahrenheit + " 'F");
 	}
 }	 
