99ffcbaf8a516e109c21c711ecf7a5a201794e4c
rudraraj100 <rudrar2002@gmail.com>
2025-12-16 21:50:04 +0530

    [Rudra Raj] : Created a program to get result of base raised to the exponent without using loops or conditional

---
+++ gcr-codebase/programming-prequisites/PowerCalculation.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+  
+  public class PowerCalculation{
+     public static void main(String[] args){
+	    Scanner sc = new Scanner(System.in);
+		int base = sc.nextInt();
+		int exponent = sc.nextInt();
+		
+		int result = Math.pow(base, exponent);
+		
+		System.out.println(result);
+	}
+}	
+     
