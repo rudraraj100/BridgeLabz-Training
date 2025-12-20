8d498c0e2486681138710354cebe89eaf6183d16
rudraraj100 <rudrar2002@gmail.com>
2025-12-20 12:29:13 +0530

    [Rudra Raj] : correct the syntax error

--- gcr-codebase/control-flows/Levelthree/DayOfWeek.java
+++ gcr-codebase/control-flows/Levelthree/DayOfWeek.java
@@ -18,12 +18,13 @@ public class DayOfWeek{
 	  int d = Integer.parseInt(args[1]);
 	  int y = Integer.parseInt(args[2]);
 	  
-	 
+	  // perform some formulas
 	  int y0 = y - (14-m)/12;
 	  int x = y0 + y0/4 - y0/100 + y0/400;
 	  int m0 = m + 12 * ((14-m)/12)-2;
 	  int d0 = (d + x + 31 * m0/12) % 7;
 	  
+		// apply switch to get the day
 		switch(d0){
 			case 1 :
 				System.out.println("Sunday");
@@ -48,6 +49,7 @@ public class DayOfWeek{
 				break;	
 	    }
 		
+	  // close scanner stream	
 	  sc.close();
 	}
 }	
