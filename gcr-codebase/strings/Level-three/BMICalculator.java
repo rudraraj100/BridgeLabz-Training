114465ea64e9c076db9286e0d2d9ea371584fd39
rudraraj100 <rudrar2002@gmail.com>
2025-12-24 08:06:00 +0530

    [Rudra Raj] : Program that calculate bmi of a person

--- gcr-codebase/strings/Level-three/BMICalculator.java
+++ gcr-codebase/strings/Level-three/BMICalculator.java
@@ -1,9 +1,13 @@
 /*
-An organization took up the exercise to find the Body Mass Index (BMI) of all the persons in a team of 10 members. For this create a program to find the BMI and display the height, weight, BMI, and status of each individual
+An organization took up the exercise to find the Body Mass Index (BMI) of all the persons in a team of 10 members. 
+For this create a program to find the BMI and display the height, weight, BMI, and status of each individual
 Hint => 
-Take user input for the person's weight (kg) and height (cm) and store it in the corresponding 2D array of 10 rows. The First Column stores the weight and the second column stores the height in cm
-Create a Method to find the BMI and status of every person given the person's height and weight and return the 2D String array. Use the formula BMI = weight / (height * height). Note unit is kg/m^2. For this convert cm to meter
-Create a Method that takes the 2D array of height and weight as parameters. Calls the user-defined method to compute the BMI and the BMI Status and stores in a 2D String array of height, weight, BMI, and status.
+Take user input for the person's weight (kg) and height (cm) and store it in the corresponding 2D array of 10 rows. 
+The First Column stores the weight and the second column stores the height in cm
+Create a Method to find the BMI and status of every person given the person's height and weight and return the 2D String array. 
+Use the formula BMI = weight / (height * height). Note unit is kg/m^2. For this convert cm to meter
+Create a Method that takes the 2D array of height and weight as parameters. 
+Calls the user-defined method to compute the BMI and the BMI Status and stores in a 2D String array of height, weight, BMI, and status.
 Create a method to display the 2D string array in a tabular format of Person's Height, Weight, BMI, and the Status
 Finally, the main function takes user inputs, calls the user-defined methods, and displays the result.
 */
