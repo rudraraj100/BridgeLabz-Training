e6fdaf2d8c04d82425e5e0b69d90489473b9097f
rudraraj100 <rudrar2002@gmail.com>
2025-12-28 20:11:01 +0530

    [Rudra Raj] : Added circle main class file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/Levelone/CircleMain.java
@@ -0,0 +1,15 @@
+package com.constructor.levelone;
+
+public class CircleMain {
+	// main method to display class circle
+	public static void main(String[] args) {
+		Circle c1 = new Circle();
+		c1.displayDetail();
+		
+		Circle c2 = new Circle(2.5f);
+		c2.displayDetail();
+		
+
+	}
+
+}
