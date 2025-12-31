569d64993e78a4e90409fd9aa1b6307458fe5549
rudraraj100 <rudrar2002@gmail.com>
2025-12-31 11:17:43 +0530

    [Rudra Raj] : Added customer file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/objectmodelling/Customer.java
@@ -0,0 +1,29 @@
+package com.objectmodelling;
+
+public class Customer {
+	
+	 private String name;
+	    private double balance;
+	    private Bank bank;   // Association (Customer linked to Bank)
+
+	    public Customer(String name) {
+	        this.name = name;
+	    }
+
+	    public void setBank(Bank bank) {
+	        this.bank = bank;
+	    }
+
+	    public void setBalance(double balance) {
+	        this.balance = balance;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    // Communication method
+	    public void viewBalance() {
+	        System.out.println(name + "'s balance in " + bank.getBankName() + " is ₹" + balance);
+	    }
+}
