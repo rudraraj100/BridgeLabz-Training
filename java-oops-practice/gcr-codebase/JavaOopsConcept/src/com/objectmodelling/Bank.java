64fb5ac912d3a15ac0865fa5737fd6576619ba03
rudraraj100 <rudrar2002@gmail.com>
2025-12-31 11:14:07 +0530

    [Rudra Raj] : Added bank file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/objectmodelling/Bank.java
@@ -0,0 +1,28 @@
+package com.objectmodelling;
+
+import java.util.ArrayList;
+public class Bank {
+	
+	 private String bankName;
+	    private ArrayList<Customer> customers;
+
+	    public Bank(String bankName) {
+	        this.bankName = bankName;
+	        customers = new ArrayList<>();
+	    }
+
+	    // Communication method
+	    public void openAccount(Customer customer, double initialBalance) {
+	        customers.add(customer);
+	        customer.setBank(this);
+	        customer.setBalance(initialBalance);
+
+	        System.out.println(customer.getName() + " opened an account in " + bankName +
+	                " with balance ₹" + initialBalance);
+	    }
+
+	    public String getBankName() {
+	        return bankName;
+	    }
+
+}
