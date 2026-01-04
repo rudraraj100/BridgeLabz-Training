6edb094619c1cc781ae0d64fd496497714dc90c1
rudraraj100 <rudrar2002@gmail.com>
2026-01-05 00:02:39 +0530

    [Rudra Raj] : Added social media friend connection system file

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcept/src/com/datastructure/linkedlist/singlylinkedlist/socialmediafriendconnection/FriendNode.java
@@ -0,0 +1,11 @@
+package com.datastructure.linkedlist.singlylinkedlist.socialmediafriendconnection;
+
+public class FriendNode {
+    int friendId;
+    FriendNode next;
+
+    public FriendNode(int friendId) {
+        this.friendId = friendId;
+        this.next = null;
+    }
+}
---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcept/src/com/datastructure/linkedlist/singlylinkedlist/socialmediafriendconnection/UserNode.java
@@ -0,0 +1,11 @@
+package com.datastructure.linkedlist.singlylinkedlist.socialmediafriendconnection;
+
+public class UserNode {
+    User data;
+    UserNode next;
+
+    public UserNode(User data) {
+        this.data = data;
+        this.next = null;
+    }
+}
