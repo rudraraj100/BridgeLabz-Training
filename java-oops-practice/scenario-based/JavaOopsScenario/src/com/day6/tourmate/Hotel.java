17c23ed8c96af53225aa9f7bf211df1af1f4846d
rudraraj100 <rudrar2002@gmail.com>
2026-01-09 10:03:17 +0530

    [Rudra Raj] : Added tour mate file

---
+++ java-oops-practice/scenario-based/JavaOopsScenario/src/com/day6/tourmate/Hotel.java
@@ -0,0 +1,18 @@
+package com.day6.tourmate;
+public class Hotel {
+    private String name;
+    private double cost;
+
+    public Hotel(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void showHotel() {
+        System.out.println("Hotel: " + name + " | Cost: ₹" + cost);
+    }
+}
---
+++ java-oops-practice/scenario-based/JavaOopsScenario/src/com/day6/tourmate/Transport.java
@@ -0,0 +1,18 @@
+package com.day6.tourmate;
+public class Transport {
+    private String type;
+    private double cost;
+
+    public Transport(String type, double cost) {
+        this.type = type;
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void showTransport() {
+        System.out.println("Transport: " + type + " | Cost: ₹" + cost);
+    }
+}
