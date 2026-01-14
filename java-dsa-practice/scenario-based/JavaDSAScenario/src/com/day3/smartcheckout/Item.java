fa60372fb6a87f00d7cde1932080f0ffe3929d4f
rudraraj100 <rudrar2002@gmail.com>
2026-01-14 21:13:53 +0530

    [Rudra Raj] : Added smart checkout file

---
+++ java-dsa-practice/scenario-based/JavaDSAScenario/src/com/day3/smartcheckout/Item.java
@@ -0,0 +1,12 @@
+package com.day3.smartcheckout;
+public class Item {
+    String name;
+    double price;
+    int stock;
+
+    public Item(String name, double price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+}
