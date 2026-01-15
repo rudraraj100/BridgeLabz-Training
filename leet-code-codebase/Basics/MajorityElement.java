7300aaf6316aa3491ba1d7dfb721b5d14bc28452
rudraraj100 <rudrar2002@gmail.com>
2026-01-15 10:15:31 +0530

    [Rudra Raj] : Added majority element file

---
+++ leet-code-codebase/Basics/MajorityElement.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/majority-element/description/
+// Problem: 169
+
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+        for(int num : nums){
+            if(count == 0){
+                candidate = num;
+            }
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+ 
+}
