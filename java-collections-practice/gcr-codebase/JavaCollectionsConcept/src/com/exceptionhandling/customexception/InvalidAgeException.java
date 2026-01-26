2f8c0d92c8aec181bfa4cb3581402646606579d2
rudraraj100 <rudrar2002@gmail.com>
2026-01-27 00:47:08 +0530

    [Rudra Raj] : Added custom exception file

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcept/src/com/exceptionhandling/customexception/InvalidAgeException.java
@@ -0,0 +1,9 @@
+
+package com.exceptionhandling.customexception;
+
+// Custom user-defined exception
+public class InvalidAgeException extends Exception {
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+}
