871574a2d6e6579472bf2fa0d8a6f5eae286261b
rudraraj100 <rudrar2002@gmail.com>
2026-01-27 01:06:00 +0530

    [Rudra Raj] : Added life cycle file

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcept/src/com/junit/lifecycle/DatabaseConnectionTest.java
@@ -0,0 +1,26 @@
+
+package com.junit.lifecycle;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+public class DatabaseConnectionTest {
+
+    DatabaseConnection db;
+
+    @BeforeEach
+    void setup() {
+        db = new DatabaseConnection();
+        db.connect();
+    }
+
+    @AfterEach
+    void cleanup() {
+        db.disconnect();
+    }
+
+    @Test
+    void testConnectionStatus() {
+        assertTrue(db.isConnected());
+    }
+}
