d661c487106e6741e16b6464a4d97cb18b276284
rudraraj100 <rudrar2002@gmail.com>
2026-01-27 01:16:22 +0530

    [Rudra Raj] : Added customloggingproxyusingreflection file

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcept/src/com/reflections/customloggingproxyusingreflection/Greeting.java
@@ -0,0 +1,5 @@
+
+package com.reflections.customloggingproxyusingreflection;
+public interface Greeting {
+    void sayHello();
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcept/src/com/reflections/customloggingproxyusingreflection/GreetingImpl.java
@@ -0,0 +1,7 @@
+
+package com.reflections.customloggingproxyusingreflection;
+public class GreetingImpl implements Greeting {
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+}
