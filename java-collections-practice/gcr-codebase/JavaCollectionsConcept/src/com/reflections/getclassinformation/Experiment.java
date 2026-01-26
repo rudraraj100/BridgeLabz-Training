6d3051c656c38ca75d331c7549c195addd7c9124
rudraraj100 <rudrar2002@gmail.com>
2026-01-27 01:17:58 +0530

    [Rudra Raj] : Added getclassinformation file

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcept/src/com/reflections/getclassinformation/Experiment.java
@@ -0,0 +1,22 @@
+
+package com.reflections.getclassinformation;
+
+public class Experiment {
+	private String name;
+
+	public Experiment(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
