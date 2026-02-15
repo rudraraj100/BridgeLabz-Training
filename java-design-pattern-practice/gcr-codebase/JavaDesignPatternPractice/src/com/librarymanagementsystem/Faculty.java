e003078033f7eb6cefd4dada74ad07bb70b631b8
rudraraj100 <rudrar2002@gmail.com>
2026-02-15 14:52:24 +0530

    [Rudra Raj] : Added library management system file

---
+++ java-design-pattern-practice/gcr-codebase/JavaDesignPatternPractice/src/com/librarymanagementsystem/Faculty.java
@@ -0,0 +1,17 @@
+
+package com.librarymanagementsystem;
+public class Faculty implements User {
+    private String name;
+
+    public Faculty(String name) {
+        this.name = name;
+    }
+
+    public void showRole() {
+        System.out.println(name + " is a Faculty.");
+    }
+
+    public void update(String msg) {
+        System.out.println(name + " notified: " + msg);
+    }
+}
